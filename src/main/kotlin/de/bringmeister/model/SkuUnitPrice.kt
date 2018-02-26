5fd888245f95fb82cd8006ed6881300d39e25df2
dannyjanz <janz.danny@gmail.com>
2018-02-26 00:48:58 -0500

    fixed style on SkuUnitPrice

--- src/main/kotlin/de/bringmeister/model/SkuUnitPrice.kt
+++ src/main/kotlin/de/bringmeister/model/SkuUnitPrice.kt
@@ -3,13 +3,7 @@ package de.bringmeister.model
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class SkuUnitPrice(
-
-        @JsonProperty("id")
-        val sku: String,
-
-        @JsonProperty("price")
-        val price: Price,
-
-        @JsonProperty("unit")
-        val unit: PackingUnit
+        @JsonProperty("id") val sku: String,
+        @JsonProperty("price") val price: Price,
+        @JsonProperty("unit") val unit: PackingUnit
 )
